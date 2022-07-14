bf25dbaae494e090b90fef897afb3066e89aa2af
vinicius.moncorvo <vinagamer9@gmail.com>
2022-07-14 09:01:15 -0300

    Update(22): Create Category Model

---
+++ BullyBookWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BullyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DysplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
