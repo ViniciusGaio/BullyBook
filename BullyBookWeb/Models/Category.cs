da4ed48976e619bfad4c5d0ecff44ba3be25ebcc
vinicius.moncorvo <vinagamer9@gmail.com>
2022-07-14 17:17:50 -0300

    Update(38): DataAnotations (Display Name, Range, etc)

--- BullyBookWeb/Models/Category.cs
+++ BullyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BullyBookWeb.Models
 {
@@ -8,7 +9,9 @@ namespace BullyBookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
+        [DisplayName("Created Date")]
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
