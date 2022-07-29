2f06d7848606d7e2f264fa5bd33e49e57ad730c9
vinicius.moncorvo <vinagamer9@gmail.com>
2022-07-29 14:29:28 -0300

    Update: Created Product model, controller and Index

---
+++ BullyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using BullyBook.DataAccess.Repository.IRepository;
+using BullyBook.Models;
+
+namespace BullyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u=> u.Id == obj.Id);
+            if(objFromDb == null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.Description = obj.Description;
+                objFromDb.Category = obj.Category;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Author = obj.Author;
+                objFromDb.FinalPrice = obj.FinalPrice;
+                objFromDb.Price = obj.FinalPrice;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.CoverTypeId = obj.CoverTypeId;
+                if(objFromDb.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
